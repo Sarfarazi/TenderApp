d0dca31e23e78dacf17b3e8f561441bbcf966007
MahshadOliaie <mahshadoliaie@gmail.com>
2025-12-04 18:43:36 +0330

    Shipment details added

--- src/components/molecules/ShipmentItem.jsx
+++ src/components/molecules/ShipmentItem.jsx
@@ -1,17 +1,11 @@
-import { Controller, useForm } from "react-hook-form"
-import BoxLayout from "../templates/BoxLayout"
-import InputGroup from "./signUpForm/InputGroup"
-import num2persian from "num2persian";
-import { useContext, useEffect, useState } from "react";
-import { useFetch } from "../../hooks/useFetch";
-import AuthContext from "../../context/AuthContext";
-import SubmitBtn from "../atoms/SubmitBtn";
+import BoxLayout from "../templates/BoxLayout";
 import ShipmentDate from "../atoms/ShipmentItem/ShipmentDate";
 import DestinationArrow from "../atoms/ShipmentItem/DestinationArrow";
-import CustomPriceTitle from "../atoms/ShipmentItem/CustomPriceTitle";
-import ShipmentStateTitle from "../atoms/ShipmentItem/ShipmentStateTitle";
 import ShipmentDesc from "../atoms/ShipmentItem/ShipmentDesc";
 import ShipmentTimer from "./ShipmentTimer";
+import ShipmentDetail from "../atoms/ShipmentItem/ShipmentDetail";
+import ShipmentPriceSection from "./ShipmentPriceSection";
+import { useState } from "react";
 
 // states:
 // 0 => available
@@ -20,99 +14,57 @@ import ShipmentTimer from "./ShipmentTimer";
 // 3 => rejected
 
 const ShipmentItem = ({ state, showToast, data: itemData }) => {
-    const { phone, token } = useContext(AuthContext)
-    const [persianPrice, setPersianPrice] = useState(num2persian(itemData.OfferPrice || 20000000))
-    const [offerPrice, setOfferPrice] = useState(0)
-    const [isExpired, setIsExpired] = useState(false)
-    const {
-        control,
-        handleSubmit,
-        formState: { isValid },
-    } = useForm({});
+  const [isExpired, setIsExpired] = useState(false);
 
-    const { refetch, resultCode, loading, error: reqError } = useFetch(
-        `https://tenapi.palaz.com/api/Main/SendTenderOffers/SendTenderOffersAsync`,
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            },
-            body: JSON.stringify({
-                "mobile": phone,
-                "tenderShippingFormID": itemData?.Id,
-                "offerPrice": offerPrice
-            }),
-        }
-    );
+  return (
+    <>
+      <BoxLayout state={state}>
+        <div className={"flex flex-col gap-5 items-center w-full "}>
+          {state == 0 && itemData?.SaveDate && itemData?.TenderDuration && (
+            <ShipmentTimer
+              setIsExpired={setIsExpired}
+              startTime={itemData?.SaveDate}
+              duration={itemData?.TenderDuration}
+            />
+          )}
 
-    const submit = (data) => {
-        setOfferPrice(+data.price)
-    }
+          <ShipmentDate content={itemData?.StartDate} />
+          <DestinationArrow
+            state={state}
+            from={itemData?.Beginning}
+            to={itemData?.Destination}
+          />
 
-    useEffect(() => {
-        if (offerPrice >= 100000) {
-            refetch()
-        }
-    }, [offerPrice])
+          <div className="self-start flex flex-col gap-2 w-full border-b pb-5">
+            {itemData?.WeightApprox && (
+              <ShipmentDetail
+                title={"وزن بار"}
+                value={itemData?.WeightApprox}
+                isWeight={true}
+              />
+            )}
+            {itemData?.LoadTypeName && (
+              <ShipmentDetail
+                title={"نوع بار"}
+                value={itemData?.LoadTypeName}
+              />
+            )}
+          </div>
 
-    useEffect(() => {
-        if (resultCode == 200) {
-            showToast(true, null, "پیشنهاد شما با موفقیت ارسال شد؛ منتظر تأیید اپراتور باشید")
-            setTimeout(() => {
-                window.location.reload();
-            }, 100);
-        }
+          {itemData?.Description && (
+            <ShipmentDesc state={state} desc={itemData?.Description} />
+          )}
 
-        if (reqError) {
-            showToast(true, reqError, null)
-        }
-    }, [resultCode, reqError])
+          <ShipmentPriceSection
+            isExpired={isExpired}
+            state={state}
+            itemData={itemData}
+            showToast={showToast}
+          />
+        </div>
+      </BoxLayout>
+    </>
+  );
+};
 
-    return (
-        <>
-            <BoxLayout state={state}>
-                <div className={"flex flex-col gap-5 items-center w-full " + `${isExpired && "pointer-events-none opacity-50"}`}>
-                    {(state == 0 && itemData?.SaveDate && itemData?.TenderDuration) && <ShipmentTimer setIsExpired={setIsExpired} startTime={itemData?.SaveDate} duration={itemData?.TenderDuration} />}
-
-                    <ShipmentDate content={itemData?.StartDate} />
-                    <DestinationArrow state={state} from={itemData?.Beginning} to={itemData?.Destination} />
-                    {itemData?.Description && <ShipmentDesc state={state} desc={itemData?.Description} />}
-
-                    <div className="w-full flex flex-col items-center justify-center gap-4">
-                        <CustomPriceTitle state={state} />
-
-
-                        {state == 0 && <Controller
-                            name="price"
-                            control={control}
-                            rules={{
-                                required: "قیمت پیشنهادی را وارد کنید",
-                                validate: (value) => {
-                                    if (value >= 100000) {
-                                        return true;
-                                    }
-                                    return "لطفاً قیمت را صحیح وارد کنید";
-                                }
-                            }}
-                            render={({ field, fieldState }) => (
-                                <InputGroup {...field} type="text" inputMode="numeric" placeholder="20000000" error={fieldState.error?.message} setPersianPrice={setPersianPrice} />
-
-                            )}
-                        />}
-
-                        {state !== 0 && <div className={"rounded-2xl w-full overflow-hidden text-center py-3 text-white " + `${(state == 2) ? "bg-Yellow" : (state == 1) ? "bg-Green" : "bg-Red"}`}><p>{itemData.OfferPrice ?? "قیمت یافت نشد"}</p></div>}
-
-                        <p className="-mt-3 opacity-60">{persianPrice} تومان</p>
-
-
-                        {state == 0 && <div className={"mt-2 w-full " + `${(isValid) ? "" : "pointer-events-none opacity-70"}`}><SubmitBtn loading={loading} context={isExpired ? "مهلت ثبت درخواست پایان یافت" : "ارسال پیشنهاد قیمت"} onClick={handleSubmit(submit)} color={"Red"} /></div>}
-                        <ShipmentStateTitle state={state} />
-                    </div>
-                </div>
-            </BoxLayout>
-        </>
-    )
-}
-
-export default ShipmentItem
+export default ShipmentItem;
--- src/components/molecules/ShipmentTimer.jsx
+++ src/components/molecules/ShipmentTimer.jsx
@@ -1,36 +1,40 @@
-import { useEffect } from "react"
-import useRemaining from "../../hooks/useRemaining"
-import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown"
-import '@leenguyen/react-flip-clock-countdown/dist/index.css';
-
+import { useEffect } from "react";
+import useRemaining from "../../hooks/useRemaining";
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
+import "@leenguyen/react-flip-clock-countdown/dist/index.css";
 
 const ShipmentTimer = ({ startTime, duration, setIsExpired }) => {
-    const remaining = useRemaining(startTime, duration)
+  const remaining = useRemaining(startTime, duration);
 
-    useEffect(() => {
-        setIsExpired(remaining.expired)
-    }, [remaining.expired])
+  useEffect(() => {
+    setIsExpired(remaining.expired);
+  }, [remaining.expired]);
 
-    return (
-        <div className="flex items-center justify-center w-full text-red-500 text-lg" dir="ltr">
+  return (
+    <div
+      className="flex items-center justify-center w-full text-red-500 text-lg"
+      dir="ltr"
+    >
+      {remaining.diff ? (
+        <FlipClockCountdown
+          to={
+            new Date().getTime() +
+            (remaining.hrs * 60 * 60 + remaining.mins * 60 + remaining.secs) *
+              1000
+          }
+          showLabels={false}
+          daysInHours={true}
+          renderMap={[false, true, true, true]}
+          digitBlockStyle={{ width: 30, height: 40, fontSize: 30 }}
+          dividerStyle={{ color: "white", height: 0 }}
+          separatorStyle={{ color: "black", size: "6px" }}
+          duration={0.5}
+        />
+      ) : (
+        <p className="text-xl">مهلت ثبت درخواست پایان یافت</p>
+      )}
+    </div>
+  );
+};
 
-            {remaining.diff ?
-                <FlipClockCountdown
-                    to={new Date().getTime() + (remaining.hrs * 60 * 60 + remaining.mins * 60 + remaining.secs) * 1000}
-                    showLabels={false}
-                    daysInHours={true}
-                    renderMap={[false, true, true, true]}
-                    digitBlockStyle={{ width: 30, height: 40, fontSize: 30 }}
-                    dividerStyle={{ color: 'white', height: 0 }}
-                    separatorStyle={{ color: 'black', size: '6px' }}
-                    duration={0.5}
-                />
-                :
-                <p className="text-xl">مهلت ثبت درخواست پایان یافت</p>
-            }
-        </div>
-
-    )
-}
-
-export default ShipmentTimer
+export default ShipmentTimer;
